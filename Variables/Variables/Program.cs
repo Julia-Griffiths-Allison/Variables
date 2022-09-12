dbbecff76ba3f2f523b6815977a101a263a8ecd8
Julia G. Allison <112894158+Julia-Griffiths-Allison@users.noreply.github.com>
2022-09-12 14:35:50 -0400

    Update Program.cs

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -4,5 +4,12 @@ string cat = "Bagheera";
 int age = 2;
 char kitty = '^';
 bool alive = true;
-double size = 24;
+double size = 24.0;
 decimal width = .75m;
+
+Console.WriteLine($"Name: {cat}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Symbol: {kitty}");
+Console.WriteLine($"Living status: {alive}");
+Console.WriteLine($"Size -inches: {size}");
+Conseole.WriteLine($"Width -feet: {width}");
